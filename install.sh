7f26764831c8a0ac46e14b70303a814b67cdb6b0
Dongkwan Kim <todoaskit@gmail.com>
2021-01-01 00:56:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 CUDA=${1:-"cu102"}
 TORCH=${2:-"1.6.0"}
 echo "CUDA: ${CUDA} / TORCH: ${TORCH}"
-pip3 install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
-pip3 install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
-pip3 install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
-pip3 install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-${TORCH}.html
+pip3 install torch==${TORCH}
+pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip3 install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip3 install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip3 install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip3 install torch-geometric
 pip3 install -r requirements.txt
 echo "Install completed"
