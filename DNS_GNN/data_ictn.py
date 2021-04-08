eeb5eb9d4521b0fe59288be18e6c560ffe7d1915
Dongkwan Kim <todoaskit@gmail.com>
2021-04-08 22:57:10 +0900

    Refactor data_ictn (ICTN to CCAminer)

--- DNS_GNN/data_ictn.py
+++ DNS_GNN/data_ictn.py
@@ -13,53 +13,55 @@ from data_base import DatasetBase
 from data_utils import CompleteSubgraph
 
 
-def load_propagation_graphs_data_split(paths, **kwargs) -> (List[Data], Dict[int, str]):
-    propagation = pickle.load(open(paths[1], "rb"))
-
-    data_train = load_propagation_graphs(propagation, 'train')
-    data_val = load_propagation_graphs(propagation, 'dev')
-    data_test = load_propagation_graphs(propagation, 'test')
-
+def load_cascades_train_val_test(paths, **kwargs) -> (List[Data], Dict[int, str]):
+    cascades = pickle.load(open(paths[1], "rb"))
+    data_train = load_cascades(cascades, 'train')
+    data_val = load_cascades(cascades, 'dev')
+    data_test = load_cascades(cascades, 'test')
     return data_train, data_val, data_test
 
 
-def load_propagation_graphs(file, file_type, **kwargs) -> (List[Data], Dict[int, str]):
+def load_cascades(file, file_type, **kwargs) -> (List[Data], Dict[int, str]):
     x_index_list = file[file_type]["x_index_list"]
     edge_index_list = file[file_type]["edge_index_list"]
-    labels = file[file_type]["ys1_list"]  # changeable ['ys1_list', 'ys2_list']
+    labels_1 = file[file_type]["ys1_list"]
+    labels_2 = file[file_type]["ys2_list"]
 
     data_list = []
-    for x_index, edge_index, y in tqdm(zip(x_index_list,
-                                           edge_index_list,
-                                           labels),
-                                       total=len(labels)):
+    for x_index, edge_index, y1, y2 in tqdm(zip(x_index_list,
+                                                edge_index_list,
+                                                labels_1,
+                                                labels_2),
+                                            total=len(labels_1)):
         x_index = torch.Tensor(x_index).long().view(-1, 1)  # [N, 1]
-        sorted_edge_index = torch.Tensor(edge_index).long()  # [2, E]
-        y = torch.Tensor([y]).long()  # [1]
-        data = Data(x=x_index,
-                    edge_index=sorted_edge_index,
-                    y=y)
+        edge_index = torch.Tensor(edge_index).long()  # [2, E]
+        y = torch.Tensor([y1, y2]).long().view(1, -1)  # [1, 2]
+        data = Data(x=x_index, edge_index=edge_index, y=y)
         data_list.append(data)
     return data_list
 
 
-class ICTN(DatasetBase):
-    """Dataset of Fake News Twitter Network"""
+class CCAMiner(DatasetBase):
+    """Dataset of Citation Cascades from ArnetMiner published in
+       DeepCas: an End-to-end Predictor of Information Cascades (WWW 2017)"""
 
     def __init__(self, root, name,
                  slice_type, slice_range: Tuple[int, int] or Tuple[float, float], num_slices,
                  val_ratio=0.15, test_ratio=0.15, debug=False, seed=42,
                  transform=None, pre_transform=None, **kwargs):
 
-        assert name in ["0.0", "0.001", "0.002", "0.003", "0.004"]
-        assert slice_type in ["time", "num_edges"]
-        super(ICTN, self).__init__(
+        assert name in ["y1", "y2"]
+        assert slice_type in ["num_edges"]
+        super(CCAMiner, self).__init__(
             root, name, slice_type, slice_range, num_slices, val_ratio, test_ratio, debug, seed,
             transform, pre_transform, **kwargs,
         )
 
     def load(self):
         self.data, self.slices = torch.load(self.processed_paths[0])
+
+        y_idx = 0 if self.name == "y1" else 1
+        self.data.y = self.data.y[:, y_idx]
 
         meta = torch.load(self.processed_paths[1])
         self.num_train = int(meta[0])
@@ -70,9 +72,7 @@ class ICTN(DatasetBase):
 
     @property
     def raw_file_names(self):
-        return ["idx_network_{}.gpickle".format(self.name),
-                "idx_propagation_{}.pkl".format(self.name),
-                "user_network_data_{}.pt".format(self.name)]
+        return ["idx_network.gpickle", "idx_cascade.pkl", "network_data.pt"]
 
     @property
     def processed_file_names(self):
@@ -98,11 +98,13 @@ class ICTN(DatasetBase):
         cprint("Loaded global_data at {}".format(self.raw_paths[2]), "green")
         print("\t- {}".format(global_data))
 
-        data_list = load_propagation_graphs_data_split(self.raw_paths)
+        data_train, data_val, data_test = load_cascades_train_val_test(self.raw_paths)
         cprint("Loaded data_list at {}".format(self.raw_paths[1]), "green")
-        print("\t- num_graphs: {}".format(len(data_list)))
+        print("\t- num_graphs: {}".format(len(data_train + data_val + data_test)))
 
-        data_train, data_val, data_test = self.process_with_slice_data_train_val_test(data_list)
+        data_train, data_val, data_test = self.process_with_slice_data_train_val_test(
+            (data_train, data_val, data_test),
+        )
 
         data_total = data_train + data_val + data_test
         if self.pre_transform is not None:
@@ -127,9 +129,9 @@ if __name__ == '__main__':
     PATH = "/mnt/nas2/GNN-DATA"
     DEBUG = False
 
-    ictn_kwargs = dict(
+    ccaminer_kwargs = dict(
         root=PATH,
-        name="0.0",  # 0.0 0.001 0.002 0.003 0.004
+        name="y1",  # y1, y2
         slice_type="num_edges",
         slice_range=(5, 10),
         num_slices=1,
@@ -139,19 +141,20 @@ if __name__ == '__main__':
         seed=42,
     )
 
-    ictn = ICTN(**ictn_kwargs)
-    ictn.save_global_data()
+    ccaminer = CCAMiner(**ccaminer_kwargs)
 
-    train_ictn, val_ictn, test_ictn = ictn.get_train_val_test()
+    train_ccaminer, val_ccaminer, test_ccaminer = ccaminer.get_train_val_test()
 
     print("Train")
-    for b in train_ictn:
+    for i, b in enumerate(train_ccaminer):
         print(b, b.num_obs_x)
-        print("------")
+        if i > 4:
+            break
 
     print("Test")
-    for b in test_ictn:
+    for i, b in enumerate(test_ccaminer):
         print(b, b.num_obs_x)
-        break
+        if i > 4:
+            break
 
-    ictn.print_summary()
+    ccaminer.print_summary()
