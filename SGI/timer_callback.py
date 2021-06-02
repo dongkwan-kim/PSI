f04b4d8989f2537b273a8c8d15b2126692567189
Dongkwan Kim <todoaskit@gmail.com>
2021-06-02 13:55:07 +0900

    Add more control variables & verbosity

--- SGI/timer_callback.py
+++ SGI/timer_callback.py
@@ -37,6 +37,10 @@ class TimerCallback(Callback):
         dt_fit_start = now - self.time_fit_start
         dt_epoch_start = now - self.time_epoch_start
         cprint("\n------------------", "green")
+        try:
+            cprint(pl_module.hparams.custom_key, "green")
+        except AttributeError:
+            pass
         cprint("Total #batch: {}".format(self.batch_count), "green")
 
         cprint("\n------------------", "green")
@@ -63,13 +67,19 @@ if __name__ == '__main__':
     main_args = get_args(
         model_name="SGI",
         dataset_name="FNTN",  # FNTN, EMUser, HPOMetab
-        custom_key="BIE2D2F64-ISI-X-GB-PGA",  # E2D2F64-X, BIE2D2F64-X-PGA, E2D2F64-ISI-X-GB, BIE2D2F64-ISI-X-GB-PGA
+        custom_key="BIE2D2F64-ISI-X-GB-PGA",
+        # E2D2F64-X, BIE2D2F64-X-PGA,
+        # SAGE-SHORT-ISI-X-GB, BISAGE-SHORT-ISI-X-GB-PGA
+        # E2D2F64-ISI-X-GB, BIE2D2F64-ISI-X-GB-PGA
     )
 
     # control variables
     main_args.data_sampler_num_workers = max(40, os.cpu_count())
     main_args.batch_size = 4
     main_args.accumulate_grad_batches = 1
+    if main_args.use_decoder:
+        main_args.data_sampler_dropout_edges = 0.5
+        main_args.pool_ratio = 0.001
 
     main_args.log_dir = "../logs_tmp"
     # main_args.model_debug = True
