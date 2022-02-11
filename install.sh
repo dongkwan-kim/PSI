506be7e28f9385d40ddb9bfae0a3bf99600e93f8
Dongkwan Kim <todoaskit@gmail.com>
2022-02-11 16:40:30 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 CUDA=${1:-"cu102"}
 TORCH=${2:-"1.6.0"}
 echo "CUDA: ${CUDA} / TORCH: ${TORCH}"
-pip3 install torch==${TORCH}
+pip3 install torch==${TORCH} -f https://download.pytorch.org/whl/${CUDA}/torch_stable.html
 pip3 install torch-scatter==2.0.5 -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip3 install torch-sparse==0.6.7 -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
 pip3 install torch-cluster==1.5.7 -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
